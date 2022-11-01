648cdccd07c0d6bcbf34c8e756f68387e0c9214b
rishavemail786 <rishavemail786@gmail.com>
2022-11-01 13:11:24 +0530

    Initial commit

---
+++ C12-S4-PC-SPRINGBOOT/src/main/java/com/jap/demo/C12S4PCSPRINGBOOT/C12S4PcSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.jap.demo.C12S4PCSPRINGBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C12S4PcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C12S4PcSpringbootApplication.class, args);
+		System.out.println("Good day too you");
+	}
+
+}
---
+++ C12-S4-PC-SPRINGBOOT/src/test/java/com/jap/demo/C12S4PCSPRINGBOOT/C12S4PcSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jap.demo.C12S4PCSPRINGBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C12S4PcSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
